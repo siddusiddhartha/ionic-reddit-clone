e25fd1e56934154dd8c2155c85a62fea7fbf7893
siddhartha <msiddu033@gmail.com>
2017-06-03 07:56:31 +0530

    Adding src folder.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,37 +4,50 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+// import { ContactPage } from '../pages/contact/contact';
+// import { HomePage } from '../pages/home/home';
+import { RedditsPage } from '../pages/reddits/reddits';
+import { SettingsPage } from '../pages/settings/settings';
+import { DetailsPage } from '../pages/details/details';
+
+
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+// services
+import { RedditService } from './services/reddits.service';
+import { HttpModule } from '@angular/http';
+
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
-    HomePage,
+    SettingsPage,
+    RedditsPage,
+    DetailsPage,
     TabsPage
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
-    HomePage,
+    SettingsPage,
+    RedditsPage,
+    DetailsPage,
     TabsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    RedditService,
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule { }
